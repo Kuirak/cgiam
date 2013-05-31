75e398dba43259e31e1b1a1a47511078585a31e8
Kuirak <newdrako@gmx.net>
2013-06-01 00:14:02 +0200

    blatt3 aufgabe 6 d)

--- Three/simpleCube/js/simpleCube.js
+++ Three/simpleCube/js/simpleCube.js
@@ -47,7 +47,7 @@ function threeStart() {
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( 800, 600 );
     document.body.appendChild( renderer.domElement );
-
+    renderer.shadowMapEnabled =true;
 
 
     var scene = new THREE.Scene();
@@ -58,7 +58,7 @@ function threeStart() {
         0.1,            // Near plane
         10000           // Far plane
     );
-    camera.position.set( -15, 10, 10 );
+    camera.position.set( -40, 10, 10 );
 
 
     scene.add( camera );
@@ -67,15 +67,33 @@ function threeStart() {
     new THREE.CubeGeometry( 5, 5, 5 ),
     new THREE.MeshLambertMaterial( { color: 0xFF0000, ambient: 0xFF0000 } )
     );
+    cube.receiveShadow =true;
     scene.add( cube );
     initDistCubeCamera =camera.position.distanceTo(cube.position);
     camera.lookAt( cube.position );
-    var light = new THREE.PointLight( 0xFFFF00 );
-    light.position.set( -15, 10, 10 );
+
+    var torus = new THREE.Mesh(
+        new THREE.TorusGeometry(10,2,10,20),
+        new THREE.MeshPhongMaterial({color:0x68366F,ambient:0x68366F })
+    );
+    torus.position.set(0,0,0);
+    torus.castShadow =true;
+    torus.receiveShadow =true;
+
+    scene.add(torus);
+    var light = new THREE.SpotLight( 0xFFFFFF );
+    light.position.set( -100, 20, 20 );
+    light.lookAt(cube.position);
+    light.castShadow=true;
+
+    light.shadowDarkness = 0.5;
+
     scene.add( light );
     renderer.domElement.onmousedown=mouseDown;
     renderer.domElement.onmouseup=mouseUp;
     renderer.domElement.onmousemove=mouseMove;
+
+
 
 
     function render() {
